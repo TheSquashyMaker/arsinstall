a8c4f885188fed6ba05c6d56cd3fe3c9951be8b4
TheSquashyMaker <49253629+TheSquashyMaker@users.noreply.github.com>
2019-04-05 13:54:56 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 GRAY='\033[1;30m'; RED='\033[0;31m'; LRED='\033[1;31m'; GREEN='\033[0;32m'; LGREEN='\033[1;32m'; ORANGE='\033[0;33m'; YELLOW='\033[1;33m'; BLUE='\033[0;34m'; LBLUE='\033[1;34m'; PURPLE='\033[0;35m'; LPURPLE='\033[1;35m'; CYAN='\033[0;36m'; LCYAN='\033[1;36m'; LGRAY='\033[0;37m'; WHITE='\033[1;37m';
 echo -n "${PURPLE}
     "Automated installation of SARNDBOX software (http://idav.ucdavis.edu/~okreylos/ResDev/SARndbox/LinkSoftwareInstallation.html)
-    Copyright (C) 2019  TheSquashyMaker
+    Copyright (C) 2019  TheSquashyMaker, Bill VanLoo, Nathan Hatt
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
