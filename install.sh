1a25a83188c26616256ec7a995c522c36b78f22c
TheSquashyMaker <49253629+TheSquashyMaker@users.noreply.github.com>
2019-04-08 14:28:21 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ echo -n "${PURPLE}
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>"
+cd $HOME
 if [ -f $HOME/src]; then
 echo -n "${YELLOW}Removing old versions"
 cd $HOME/src
